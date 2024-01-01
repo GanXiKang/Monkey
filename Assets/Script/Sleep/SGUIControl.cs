ecb237e3702551a9468ae0d4f699be87344658bf
GanXiKang <gan0612xk@hotmail.com>
2024-01-02 01:48:29 +0800

    feat : new script

---
+++ Assets/Script/Sleep/SGUIControl.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SGUIControl : MonoBehaviour
+{
+    public GameObject
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
