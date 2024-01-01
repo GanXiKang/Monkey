d3539df7b2ce152b3e779d6880687a6a26fe4958
GanXiKang <gan0612xk@hotmail.com>
2024-01-02 01:58:05 +0800

    feat : 編寫code

--- Assets/Script/Sleep/SGUIControl.cs
+++ Assets/Script/Sleep/SGUIControl.cs
@@ -6,15 +6,34 @@ using UnityEngine.SceneManagement;
 
 public class SGUIControl : MonoBehaviour
 {
-    public GameObject
+    public GameObject main, esc;
+
     void Start()
     {
-        
+        main.SetActive(true);
+        esc.SetActive(false);
     }
 
     
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            esc.SetActive(false);
+            main.SetActive(true);
+            Time.timeScale = 0f;
+        }
+    }
+
+    public void Button_Continue()
+    {
+        main.SetActive(true);
+        esc.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void Button_Exit()
+    {
+        SceneManager.LoadScene(0);
     }
 }
