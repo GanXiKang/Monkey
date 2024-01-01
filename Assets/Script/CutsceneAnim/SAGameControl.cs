8bb5cde3f4af386a3f457d0bcddd0d2302415b56
GanXiKang <gan0612xk@hotmail.com>
2024-01-02 02:43:58 +0800

    feat : 編寫code

--- Assets/Script/CutsceneAnim/SAGameControl.cs
+++ Assets/Script/CutsceneAnim/SAGameControl.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SAGameControl : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
+        Invoke("NextScene", 15f);
     }
 
-    // Update is called once per frame
-    void Update()
+    void NextScene()
     {
-        
+        SceneManager.LoadScene(4);
     }
 }
