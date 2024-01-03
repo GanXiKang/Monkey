e373162ce34f6c5b3d1cf53a4b0eeb48e91d87aa
GanXiKang <gan0612xk@hotmail.com>
2024-01-04 01:21:43 +0800

    feat ： 編寫code

--- Assets/Script/Sleep/SGUIControl.cs
+++ Assets/Script/Sleep/SGUIControl.cs
@@ -6,16 +6,24 @@ using UnityEngine.SceneManagement;
 
 public class SGUIControl : MonoBehaviour
 {
+    public static int barScore;
+
     public GameObject main, esc;
+    public Image power;
+
+    float barLength;
 
     void Start()
     {
+        barScore = 3;
+
         main.SetActive(true);
         esc.SetActive(false);
     }
 
     void Update()
     {
+        BarControl();
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             esc.SetActive(true);
@@ -33,5 +41,12 @@ public class SGUIControl : MonoBehaviour
     public void Button_Exit()
     {
         SceneManager.LoadScene(0);
+    }
+
+    void BarControl()
+    {
+        barLength = (float)barScore / 20;
+        barLength *= 5;
+        power.rectTransform.localScale = new Vector3(barLength, 1, 1);
     }
 }
