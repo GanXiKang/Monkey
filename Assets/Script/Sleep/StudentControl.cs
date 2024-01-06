b8146a936fd7e4f82c2c02607b66c8dd0e3738a7
GanXiKang <gan0612xk@hotmail.com>
2024-01-06 20:39:11 +0800

    feat ： 編寫動作

--- Assets/Script/Sleep/StudentControl.cs
+++ Assets/Script/Sleep/StudentControl.cs
@@ -6,15 +6,29 @@ public class StudentControl : MonoBehaviour
 {
     Animator anim;
 
+    public static bool isFright;
+
     void Start()
     {
         anim = GetComponent<Animator>();
 
         anim.SetBool("Idle_Sitting", true);
+        anim.SetBool("GoSleep", true);
+        anim.SetBool("Sleeping", true);
+
+        isFright = false;
     }
 
     void Update()
     {
-        
+        if (isFright)
+        {
+            anim.SetBool("Frightened", true);
+            isFright = false;
+        }
+        else
+        {
+            anim.SetBool("Frightened", false);
+        }
     }
 }
