83c1e2fa7f5631e1577e65fa1e0ccf737bdc7f9f
GanXiKang <gan0612xk@hotmail.com>
2024-01-06 20:46:27 +0800

    feat ： 編寫特效

--- Assets/Script/Sleep/TeacherControl.cs
+++ Assets/Script/Sleep/TeacherControl.cs
@@ -6,15 +6,26 @@ public class TeacherControl : MonoBehaviour
 {
     Animator anim;
 
+    public GameObject effect;
+
+    public static bool isAngryEffects;
+
     void Start()
     {
         anim = GetComponent<Animator>();
 
         anim.SetBool("T_IdleSitting", true);
+        effect.SetActive(false);
+        isAngryEffects = false;
     }
 
     void Update()
     {
+        if (isAngryEffects)
+        {
+            effect.SetActive(true);
+        }
+
         if (SGUIControl.isLose)
         {
             anim.SetBool("T_IdleSitting", false);
