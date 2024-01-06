8dbe090cd54d2983dd86ea99a63c293acbee4407
GanXiKang <gan0612xk@hotmail.com>
2024-01-07 02:17:37 +0800

    feat ： copy code

--- Assets/Script/Run/FloorControl.cs
+++ Assets/Script/Run/FloorControl.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class FloorControl : MonoBehaviour
 {
+    public GameObject[] map = new GameObject[5];
+
+    int colliderNum;
+
     void Start()
     {
-        
+        colliderNum = 0;
     }
 
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.tag == "Player")
+        {
+            colliderNum++;
+            if (colliderNum == 1)
+            {
+                int s = Random.Range(0, 5);
+                Instantiate(map[s], transform.position + new Vector3(0, 0, -250), transform.rotation);
+            }
+            else
+            {
+                Destroy(gameObject, 2f);
+            }
+        }
     }
 }
