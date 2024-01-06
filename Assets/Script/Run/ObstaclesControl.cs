42979e93098c0aafdcf4e6f7ba8597a2265d40d9
GanXiKang <gan0612xk@hotmail.com>
2024-01-06 22:57:21 +0800

    feat ： 編寫音效

--- Assets/Script/Run/ObstaclesControl.cs
+++ Assets/Script/Run/ObstaclesControl.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class ObstaclesControl : MonoBehaviour
 {
+    public AudioSource BGM;
+    public AudioClip coll;
+
     void Start()
     {
         
@@ -19,6 +22,7 @@ public class ObstaclesControl : MonoBehaviour
         if (other.tag == "Player")
         {
             MonkeyControl.isGameOver = true;
+            BGM.PlayOneShot(coll);
         }
     }
 }
