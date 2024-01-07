f6e1605641f731f9ba7eb403be34671255004a2e
GanXiKang <gan0612xk@hotmail.com>
2024-01-08 02:10:45 +0800

    feat ： 編寫code

--- Assets/Script/Menu/MenuControl.cs
+++ Assets/Script/Menu/MenuControl.cs
@@ -11,8 +11,13 @@ public class MenuControl : MonoBehaviour
     public AudioClip button;
     public GameObject main, ranking, setting;
     public GameObject[] top = new GameObject[3];
+    public Text[] playerName = new Text[3];
+    public Text[] playerScore = new Text[3];
+    public Text[] playerCombo = new Text[3];
 
     public static bool isRanking = false;
+    public static string n;
+    public static int score, combo;
 
     void Start()
     {
@@ -30,6 +35,27 @@ public class MenuControl : MonoBehaviour
             isRanking = false;
         }
         setting.SetActive(false);
+
+        switch (EndGameControl.playNum)
+        {
+            case 1:
+                playerName[0].text = n.ToString();
+                playerScore[0].text = score.ToString();
+                playerCombo[0].text = combo.ToString();
+                break;
+
+            case 2:
+                playerName[1].text = n.ToString();
+                playerScore[1].text = score.ToString();
+                playerCombo[1].text = combo.ToString();
+                break;
+
+            case 3:
+                playerName[2].text = n.ToString();
+                playerScore[2].text = score.ToString();
+                playerCombo[2].text = combo.ToString();
+                break;
+        }
     }
 
     public void Button_Start()
